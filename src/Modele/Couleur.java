107167659d0fc074759a328855629774dfa5124a
Mathias Novel <novelm@f201-07.e-im2ag.ujf-grenoble.fr>
2017-05-04 16:09:27 +0200

    Ajout type enum couleur

---
+++ src/Modele/Couleur.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author novelm
+ */
+public enum Couleur {
+    Noir("\u001B[30m", "\u001B[40m", Color.BLACK),
+    Rouge("\u001B[31m", "\u001B[41m", Color.RED),
+    Bleu("\u001B[34m", "\u001B[44m", Color.BLUE),
+    Vert("\u001B[32m", "\u001B[42m", Color.GREEN),
+    Jaune("\u001B[33m", "\u001B[43m", Color.YELLOW);
+
+    private String colorAccessible;
+    private String colorBackground;
+    private Color couleur;
+
+    Couleur(String colorAccessible, String colorBackground, Color couleur) {
+        this.colorAccessible = colorAccessible;
+        this.colorBackground = colorBackground;
+        this.couleur = couleur;
+    }
+
+    @Override
+    public String toString() {
+        return colorAccessible;
+    }
+
+    public String getColorAccessible() {
+        return colorAccessible;
+    }
+
+    public String getColorBackground() {
+        return colorBackground;
+    }
+
+    public Color getCouleur() {
+        return couleur;
+    }
+}
